d7ced0d0cc1adb98071b40603d4d3291abf376dc
Devrin Anggun Saputri <devrinanggunsaputri90@gmail.com>
2025-05-27 20:55:21 +0700

    fix

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,20 +1,23 @@
+package src.main.java;
+
 public class Counter {
+
     private int count;
 
     public Counter() {
-        count = 0;
-    }
-
-    public void increment() {
-        count++;
-    }
-
-    public void decrement() {
-        count--;
+        this.reset();
     }
 
     public void reset() {
         count = 0;
+    }
+
+    public void increment() {
+        count++; // Perbaikan
+    }
+
+    public void decrement() {
+        count--;
     }
 
     public int getCount() {
--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,36 +1,42 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Before;
+package src.test.java;
+
+import static org.junit.Assert.*;
+import src.main.java.*;
 import org.junit.Test;
 
 public class CounterTest {
-    private Counter counter;
 
-    @Before
-    public void setUp() {
-        counter = new Counter();
-    }
+	@Test
+	public void testReset() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 0; i <10; i++) {
+			testCounter.increment();
+		}
+		
+		testCounter.reset();
+		
+		assertEquals(testCounter.getCount(), 0);
+	}
 
-    @Test
-    public void testReset() {
-        counter.increment();
-        counter.increment();
-        assertEquals(2, counter.getCount());
-        counter.reset();
-        assertEquals(0, counter.getCount());
-    }
+	@Test
+	public void testIncrement() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 1; i <10; i++) {
+			testCounter.increment();
+			assertEquals(testCounter.getCount(), i);
+		}
+	}
 
-    @Test
-    public void testIncrement() {
-        counter.increment();
-        counter.increment();
-        assertEquals(2, counter.getCount());
-    }
+	@Test
+	public void testDecrement() {
+		Counter testCounter = new Counter();
+		
+		for(int i = 1; i <10; i++) {
+			testCounter.decrement();
+			assertEquals(testCounter.getCount(), i * -1);
+		}
+	}
 
-    @Test
-    public void testDecrement() {
-        counter.increment();
-        counter.increment();
-        counter.decrement();
-        assertEquals(1, counter.getCount());
-    }
 }
